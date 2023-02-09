8f8556113bb9d3470a0815b42c13adf0c9870e9c
Vale <oirelav95@gmail.com>
2023-02-09 15:53:07 +0100

    aggiungo metodi per modificare informazioni progetto

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -44,7 +44,7 @@ class MainController extends Controller
             'description' => 'nullable|string',
             'main_image' => 'url|unique:projects,main_image',
             'release_date' => 'date|before:today',
-            'repo_link' => 'required|unique:projects,repo_link|string',
+            'repo_link' => 'required||unique:projects,repo_link|string',
 
         ]);
 
@@ -59,5 +59,34 @@ class MainController extends Controller
         $project -> save();
 
         return redirect() -> route('home');
+    }
+
+    public function projectEdit(Project $project) {
+        
+        return view('pages.projectEdit', compact('project'));
+    }
+
+    public function projectUpdate(Request $request, Project $project) {
+
+        $data = $request -> validate([
+
+            'name' => 'required|unique:projects,name|string|max:64',
+            'description' => 'nullable|string',
+            'main_image' => 'url|unique:projects,main_image',
+            'release_date' => 'date|before:today',
+            'repo_link' => 'required||unique:projects,repo_link|string',
+
+        ]);
+
+        $project -> name = $data['name'];
+        $project -> description = $data['description'];
+        $project -> main_image = $data['main_image'];
+        $project -> release_date = $data['release_date'];
+        $project -> repo_link = $data['repo_link'];
+
+        $project -> save();
+
+        return redirect() -> route('home');
+
     }
 }
