029c6a31ff22e2bd05de7a0ece7fa99ff36008c6
Vale <oirelav95@gmail.com>
2023-02-09 15:55:59 +0100

    creo file projectEdit

---
+++ resources/views/pages/projectEdit.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+    <h1>
+        Edit Project
+    </h1>
+
+    <form method="POST" action="{{ route('project.update', $project) }}">
+        @csrf
+
+        <label for="name">Name: </label>
+        <input type="text" name="name" value="{{ $project -> name}}">
+        <br>
+        <label for="description">Description: </label>
+        <textarea name="description" cols="30" rows="2"></textarea>
+        <br>
+        <label for="main_image">Image URL: </label>
+        <input type="url" name="main_image" value="{{ $project -> main_image}}">
+        <br>
+        <label for="release_date">Release Date: </label>
+        <input type="date" name="release_date" value="{{ $project -> release_date}}">
+        <br>
+        <label for="repo_link">Repo Link: </label>
+        <input type="url" name="repo_link" value="{{ $project -> repo_link}}">
+        <br>
+        <input type="submit" value="UPDATE">
+    
+    </form>
+
+@endsection
