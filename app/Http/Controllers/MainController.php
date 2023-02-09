5447ef538db7a83f1e22a27d372d3dc4bf741cfe
Vale <oirelav95@gmail.com>
2023-02-09 15:50:11 +0100

    aggiungo metodi per home e privateHome in MainController

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+
+class MainController extends Controller
+{
+    public function home() {
+
+        $projects = Project::all();
+
+        return view('pages.home', compact('projects'));
+    }
+
+    public function privateHome() {
+        return view('pages.private-home');
+    }
+}
