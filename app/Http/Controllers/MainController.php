8652e9ab7c3a3a4df9e62a6c34c9ac478c6d39cd
Vale <oirelav95@gmail.com>
2023-02-09 15:52:00 +0100

    aggiungo metodi per creare nuovo progetto

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -30,4 +30,34 @@ class MainController extends Controller
 
         return redirect() -> route('home');
     }
+
+    public function projectCreate() {
+        
+        return view('pages.projectCreate');
+    }
+
+    public function projectStore(Request $request) {
+
+        $data = $request -> validate([
+
+            'name' => 'required|unique:projects,name|string|max:64',
+            'description' => 'nullable|string',
+            'main_image' => 'url|unique:projects,main_image',
+            'release_date' => 'date|before:today',
+            'repo_link' => 'required|unique:projects,repo_link|string',
+
+        ]);
+
+        $project = new Project();
+
+        $project -> name = $data['name'];
+        $project -> description = $data['description'];
+        $project -> main_image = $data['main_image'];
+        $project -> release_date = $data['release_date'];
+        $project -> repo_link = $data['repo_link'];
+
+        $project -> save();
+
+        return redirect() -> route('home');
+    }
 }
