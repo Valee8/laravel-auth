8ed3b5bb34b475239aeeeb8b56dd36b03879399f
Vale <oirelav95@gmail.com>
2023-02-11 18:59:56 +0100

    modifico title

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio</title>
+    <title>Laravel Auth</title>
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -16,6 +16,5 @@
     @include('components.header')
     @include('components.errors')
     @yield('content')
-    @include('components.footer')
 </body>
 </html>
